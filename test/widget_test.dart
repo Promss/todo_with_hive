f841b021dacaad51fa98a1ce69a34abfd5be9407
Promss <tima.nurlanbekov@gmail.com>
2023-04-25 15:00:35 +0600

    refactoring code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todo_hive/main.dart';
+import 'package:todo_hive/widget/app/myApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
