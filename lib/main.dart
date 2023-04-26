a1e2af58a6407479d47ad4a89fd1daf81832274e
Promss <tima.nurlanbekov@gmail.com>
2023-04-26 12:39:05 +0600

    connected hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_hive/widget/app/myApp.dart';
 
-void main(List<String> args) {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter;
   runApp(const MyApp());
 }
